437675093e0549773013f73e82b4df9277016f1a
P. L. Lim <2090236+pllim@users.noreply.github.com>
2021-01-11 14:37:39 -0500

    DOC: Fix docstring

--- corazon/pipeline_search_vet.py
+++ corazon/pipeline_search_vet.py
@@ -3,6 +3,6 @@ __all__ = ['search_and_vet_one']
 
 def search_and_vet_one(ticid, sector, config, vetter_list, plot=True):
     """
-    Search and vet one ticid usin gyour config and vetter list
+    Search and vet one TIC ID using your config and vetter list.
     """
     # TODO: Implement me
