3810bdd2964da1c34a9cda690b400967ce4c0982
Matt Dallas <110625433+m-dallas@users.noreply.github.com>
2025-04-29 10:40:54 -0400

    Better TGLC cleaning and workflow   (#14)
    * Simplified corazon to work with TGLC lightcuves stored in s3 buckets
    
    * Fixed LEO
    
    * Added tglc cal errors
    
    * Removed detrending outside of TGLC. Made TGLC quality flags default

--- corazon/pipeline.py
+++ corazon/pipeline.py
@@ -1,56 +1,14 @@
-__all__ = ['search_and_vet_one', 'vet_tce','vet_all_tces','get_disposition',
-           'load_def_config','load_def_vetter']
+__all__ = ['search_and_vet_one', 'vet_tce','vet_all_tces']
 
 import corazon.planetSearch as ps
-import corazon.gen_lightcurve as genlc
 import matplotlib.pyplot as plt
 import exovetter.tce as TCE
 import astropy.units as u
 import exovetter.const as const
 import lightkurve as lk
-from exovetter import vetters
-
-def load_def_config():
-    """
-    Get the default configuration dictionary.
-    Returns
-    -------
-    config : dict
-       dictionary of default values that are required to run corazon pipeline
-
-    """
-    
-    config = dict()
-    
-    config = {
-        "det_window" : 65,
-        "noise_window" : 27,
-        "n_sigma" : 4.5,  #noise reject sigma
-        "max_period_days" : 10,
-        "min_period_days" : 0.8,
-        "bls_durs_hrs" : [1,2,4,8,12],
-        "minSnr" : [1],
-        "maxTces" : 20,
-        "fracRemain" : 0.7
-        }
-    
-    return config
-    
-def load_def_vetter():
-    """
-    Load default vetter list of vetters to run.
-    """
-    
-    vetter_list = [vetters.Lpp(),
-                   vetters.OddEven(),
-                   vetters.TransitPhaseCoverage(),
-                   vetters.Sweet()]
-    
-    return vetter_list
 
 
-def search_and_vet_one(ticid, sector, lcdata, config, vetter_list,
-                       thresholds, plot=True):
+def search_and_vet_one(ticid, sector, lcdata, config, vetter_list, plot=True):
     """
     Search and vet one ticid using config and vetter list
     
@@ -80,17 +38,14 @@ def search_and_vet_one(ticid, sector, lcdata, config, vetter_list,
     
     """
     
+    # Perform gapping on the light curve:
     time = lcdata['time'].value
     flux = lcdata['flux'].value
     flags = lcdata['quality'].value
 
-    good_time, meddet_flux = ps.clean_timeseries(time, flux, flags,
-                                          config["det_window"], 
-                                          config["noise_window"], 
-                                          config["n_sigma"], 
-                                          sector)
+    good_time, meddet_flux = ps.clean_timeseries(time, flux, flags, sector)
         
-    
+    # Run BLS
     tce_list, stats = ps.identifyTces(good_time, meddet_flux, 
                                       bls_durs_hrs=config["bls_durs_hrs"],
                                       minSnr=config["minSnr"], 
@@ -100,68 +55,64 @@ def search_and_vet_one(ticid, sector, lcdata, config, vetter_list,
                                       maxP=config["max_period_days"])
     
     if plot:
-        plot_lc_tce(ticid, tce_list, time, flux, flags, good_time, 
-                    meddet_flux, stats, sector)
+        plot_lc_tce(ticid, time, flux, good_time, meddet_flux+1, flags, stats, sector)
     
     lcformat = lcdata['time'].format
-    tce_lc = lk.LightCurve(time=good_time, flux=meddet_flux+1,
-                        time_format=lcformat, meta={'sector':sector})
-    
-    result_strings, disp, reason, metrics_list, tce_tces = vet_all_tces(tce_lc, 
-                                                    tce_list, ticid, 
-                                                    vetter_list, thresholds,
-                                                    plot=False)
+    flux_err = lcdata['flux_err'].value[0:len(good_time)]
+
+    tce_lc = lk.LightCurve(time=good_time, flux=meddet_flux+1, flux_err=flux_err, time_format=lcformat, meta={'sector':sector})
+    # tce_lc.to_fits(path="./test_lc.fits", overwrite=True) # Uncomment if you want to save the actually searched lightcurve (the cleaned TGLC one)
+
+    result_strings, metrics_list, tce_tces = vet_all_tces(tce_lc, tce_list, ticid, vetter_list, plot=False)
     
     return tce_tces, result_strings, metrics_list
 
+def vet_all_tces(lc, tce_dict_list, ticid, vetter_list, plot=False):
+    lcformat = lc['time'].format
+    result_list = []
+    metrics_list = []
+    tce_list = []
+    pn = 1
+    for item in tce_dict_list:
+        tce = TCE.Tce(period = item[0]*u.day, epoch=item[1]*u.day, 
+                      depth=item[2] * const.frac_amp,
+                      duration=item[3]*u.day, 
+                      epoch_offset=const.string_to_offset[lcformat],
+                      snr=item[4],
+                      target = f"TIC {ticid}",
+                      sector = lc.sector,
+                      event = f"{pn}")
+
+
+        metrics = vet_tce(tce, lc, vetter_list, plot=plot) # dictionary of all vetting metrics
+
+        result_string = make_result_string(tce)
+
+        tce_list.append(tce)
+        result_list.append(result_string)
+        metrics_list.append(metrics)
+        pn=pn+1
+        
+    return result_list, metrics_list, tce_list
 
 def vet_tce(tce, tce_lc, vetter_list, plot=False):
-
+    """Create a dictionary with all vetting parameters returned by each vetter in vetter_list"""
     metrics = dict()
     for v in vetter_list:
         vetter = v
         
         try:
-            _ = vetter.run(tce, tce_lc)
+            vetter_dict = vetter.run(tce, tce_lc)
         except ValueError:
+            print('Did not Vet')
             pass
         if plot:
             vetter.plot()
-        metrics.update(vetter.__dict__)
+        metrics.update(vetter_dict)
         
     return metrics
 
-def get_disposition(metrics, thresholds):
-    """Apply thresholds to get a passfail"""
-    
-    disp = 'PASS' 
-    reason = ''
-    if metrics['snr'] < thresholds['snr']:
-        disp = 'FAIL'
-        reason = reason + "-LowSNR-"
-    if metrics['norm_lpp'] > thresholds['norm_lpp']:
-        disp = 'FAIL'
-        reason = reason + "-NormLPP-"
-    if metrics['tp_cover'] < thresholds['tp_cover']:
-        disp = 'FAIL'
-        reason = reason + "-PoorTransitCoverage-"
-    if metrics['oe_sigma'] > thresholds['oe_sigma']:
-        disp = 'FAIL'
-        reason = reason + "-OddEvenDetected-"
-    if metrics['sweet']['amp'][0, -1] > thresholds['sweet']:
-        disp = 'FAIL'
-        reason = reason + "-SWEETHalfPeriod"
-    if metrics['sweet']['amp'][1, -1] > thresholds['sweet']:
-        disp = 'FAIL'
-        reason = reason + "-SWEETAtPeriod"
-    if metrics['sweet']['amp'][2,-1] > thresholds['sweet']:
-        disp = 'FAIL'
-        reason = reason + "-SWEETTwicePeriod-"
-    
-    
-    return disp,reason
-    
-def make_result_string(tce, disposition, reason):
+def make_result_string(tce):
     """
    Create a string that summarizes the TCE and its disposition
     Parameters
@@ -178,85 +129,48 @@ def make_result_string(tce, disposition, reason):
     None.
 
     """
-    st = "%s, %s, %i, %8.4f, %9.4f, %8.3f, %5.3f, %5.2f, %s, %s\n" % \
+    st = "%s, %s, %i, %8.4f, %9.4f, %8.3f, %5.3f, %5.2f\n" % \
                                         (tce['target'], tce['event'],
                                                tce['sector'],
                                                tce['period'].value, 
                                                tce['epoch'].value,
                                                tce['depth'].value*1e6,
                                                tce['duration'].value*24.0,
-                                               tce['snr'],
-                                               disposition, reason)             
+                                               tce['snr'])
+
     return st
 
-
-def vet_all_tces(lc, tce_dict_list, ticid, vetter_list, thresholds, plot=False):
-    lcformat = lc['time'].format
-    disp_list = []
-    reason_list = []
-    result_list = []
-    metrics_list = []
-    tce_list = []
-    pn = 1
-    for item in tce_dict_list:
-        tce = TCE.Tce(period = item[0]*u.day, epoch=item[1]*u.day, 
-                      depth=item[2] * const.frac_amp,
-                      duration=item[3]*u.day, 
-                      epoch_offset=const.string_to_offset[lcformat],
-                      snr=item[4],
-                      target = f"TIC {ticid}",
-                      sector = lc.sector,
-                      event = f"{pn}")
-        
-        metrics = vet_tce(tce, lc, vetter_list, plot=plot)
-        metrics['snr'] = tce['snr']
-        disposition, reason = get_disposition(metrics, thresholds)
-        result_string = make_result_string(tce, disposition, reason)
-        tce['disposition'] = disposition
-        tce['reason'] = reason
-        tce_list.append(tce)
-        disp_list.append(disposition)
-        reason_list.append(reason)
-        result_list.append(result_string)
-        metrics_list.append(metrics)
-        pn=pn+1
-        
-        
-    return result_list, disp_list, reason_list, metrics_list, tce_list
-
     
-def plot_lc_tce(ticid, tce_list, time, flux, flags, good_time, 
-                good_flux, stats, sector):
-    col = ['tab:orange','tab:green','tab:purple','tab:brown',
-               'gold','magenta','lightpink']
-    plt.figure(figsize=(10,6))
-    plt.subplot(211)
-    plt.plot(good_time, good_flux,'.')
-    plt.title("Lightcurve for TIC %i in S%i" % (int(ticid), int(sector)))
-   
-    axes = plt.gca()
-    y_min, y_max = axes.get_ylim()
-    x_min, x_max = axes.get_xlim()
+def plot_lc_tce(ticid, time, flux, cleaned_time, cleaned_flux, flags, stats, sector):
+    col = ['tab:orange','tab:green','tab:purple','tab:brown', 'gold','magenta','lightpink']
+    fig, (ax1,ax2,ax3) = plt.subplots(nrows=3,ncols=1,figsize=(10,9), sharex=True)
+    
+    
+    ax1.plot(time, flux, label='Original lc', color='tab:red')
+    ax2.plot(cleaned_time, cleaned_flux, label='BLS searched cleaned lc', color='tab:blue')
+    ax3.plot(cleaned_time, cleaned_flux, color='tab:blue')
+    
+    #plt.plot(time[flags!=0], flux[flags!=0],'o', ms=3, label='flagged')
+
     for n,s in enumerate(stats):
-        plt.vlines(stats[n]['transit_times'], y_min, y_max, 
-                   colors=col[n], zorder=1, label=str(n+1))
-    plt.legend()
-    plt.subplot(212)
-    plt.plot(time, flux,'.', label="original lc")
-    plt.plot(time[flags!=0], flux[flags!=0],'o', ms=3, label='flagged')
-    plt.legend()
-    plt.xlim(x_min, x_max)
+        labeled=False
+        for transit_time in stats[n]['transit_times']:
+            ax3.axvline(x=transit_time, color=col[n], zorder=1, label=f'BLS TCE {str(n+1)}' if not labeled else '__nolegend__', alpha=0.7, ls='-' if n%2==0 else '--')
+            labeled=True
 
-def open_output_file(filename, headerlist, thresholds):
+
+    for ax in (ax1,ax2,ax3):
+        ax.legend()
+    
+    fig.suptitle("Lightcurve for TIC %i in S%i" % (int(ticid), int(sector)))
+
+def open_output_file(filename, headerlist):
     fobj = open(filename, 'a')
-    
-    fobj.write("# thresholds: " + str(thresholds)+"\n")
-    
+
     header = headerlist[0]
     for h in headerlist[1:]:
         header = header + ", " + h
-        
+
     fobj.write(header + '\n')
     
     return fobj
-
--- corazon/run_pipeline.py
+++ corazon/run_pipeline.py
@@ -4,11 +4,14 @@ import os
 from exovetter import vetters
 import matplotlib.pyplot as plt
 import corazon.gen_lightcurve as genlc
+import s3fs
+import json
+from fsspec.implementations.local import LocalFileSystem
+from astropy.utils.misc import JsonCustomEncoder
 #sys.path[2] = '/Users/smullally/Python_Code/lightkurve/lightkurve'
 
 
-def run_write_one(ticid, sector, out_dir, lc_author = 'qlp',local_dir = None,
-               run_tag = None, config_file = None, plot=False):
+def run_write_one(ticid, s3_location, sector, out_dir, lc_author = 'TGLC', config_file = None, run_tag = None, local=True, plot=False):
     """
     Run the full bls search on a list of ticids stored in a file.
 
@@ -16,18 +19,22 @@ def run_write_one(ticid, sector, out_dir, lc_author = 'qlp',local_dir = None,
     ----------
     ticid : int
        tess input catalog number
+    s3_location : str
+        string of MAST s3 location of TGLC lightcurve
     sector : int
-       tess sector to search
+       tess sector of the data being used
     out_dir : string
         directory to store all the results. One dir per ticid will be created.
     lc_author : string
-        'qlp' or 'tess-spoc'
-    local_dir : string
-        defaul is None and then pulls data from MAST API. Otherwise contains
-        directory name for the location of the data files.
+        Currently using TGLC lightcurves
+    config_file : Dictionary
+        Dictionary of gapping values
     run_tag : string, optional
         directory name and string to attach to output file names. 
-
+    local : bool
+        Specifies the file system to use
+    plot : bool
+        create plot of the pre and post cleaned lightcurve corazon ran on
     Returns
     -------
     None.
@@ -41,11 +48,14 @@ def run_write_one(ticid, sector, out_dir, lc_author = 'qlp',local_dir = None,
     if config_file is None:
         config = load_def_config()
     else:
-        print("Not implememted read in config file")
-        #config = pipeline.load_config_file()
+        config = config_file
     
-    vetter_list = load_def_vetter()
-    thresholds = load_def_thresholds()
+    if not local:
+        fs = s3fs.S3FileSystem(anon=False, profile="default")
+    else:
+        fs = LocalFileSystem()
+
+    vetter_list = [vetters.LeoTransitEvents(), vetters.Sweet(), vetters.TransitPhaseCoverage()]
     
     
     target_dir = "/tic%09is%02i/" % (int(ticid), sector)
@@ -67,25 +77,32 @@ def run_write_one(ticid, sector, out_dir, lc_author = 'qlp',local_dir = None,
         
     try:
         
-        lcdata = genlc.hlsp(ticid, sector, author=lc_author,local_dir = local_dir)
-        if lc_author == 'qlp':
-            lcdata['quality'] = lcdata['quality'].value & 2237
-         
-        tce_list, result_strings, metrics_list = pipeline.search_and_vet_one(ticid, 
-                                sector, lcdata, config, 
-                                vetter_list, thresholds, plot=plot)
+        lcdata = genlc.tglc_from_S3(s3_location)
         
+        tce_list, result_strings, metrics_list = pipeline.search_and_vet_one(ticid, sector, lcdata, config, vetter_list, plot=plot)
+
         if plot:
             plotfilename = "tic%09i-%s-plot.png" % (ticid, 
                                                     run_tag)
             plt.savefig(out_dir + target_dir + plotfilename, bbox_inches='tight')
             plt.close()
         
-        output_obj = open(output_file, 'w')
-        for r in result_strings:
-            output_obj.write(r)
-    
-        output_obj.close()
+        with fs.open(output_file, 'w') as fp: 
+            fp.write('Target, BLS_event, Sector, BLS_period, BLS_epoch, BLS_depth, BLS_duration, BLS_snr, LEO_MES, LEO_SHP, LEO_CHI, LEO_med_chases, LEO_mean_chases, LEO_max_SES, LEO_DMM, SWEET_halfperiod, SWEET_period, SWEET_2period, transit_phase_coverage\n')
+            for i,r in enumerate(result_strings):
+                newstr = ", %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n" % (metrics_list[i]['MES'],
+                                           metrics_list[i]['SHP'],
+                                           metrics_list[i]['CHI'],
+                                           metrics_list[i]['med_chases'],
+                                           metrics_list[i]['mean_chases'],
+                                           metrics_list[i]['max_SES'],
+                                           metrics_list[i]['DMM'],
+                                           metrics_list[i]['amp'][2][0], # last array in Sweet (amplitude to uncertainty ratio): half-period
+                                           metrics_list[i]['amp'][2][1], # period
+                                           metrics_list[i]['amp'][2][2], # twice the period
+                                           metrics_list[i]['transit_phase_coverage'])
+                newr = r[:-1]+newstr
+                fp.write(newr)
         
         #Write TCEs
         for tce in tce_list:
@@ -95,29 +112,17 @@ def run_write_one(ticid, sector, out_dir, lc_author = 'qlp',local_dir = None,
     
             full_filename = out_dir + target_dir + tcefilename
             tce['lc_author'] = lc_author
-            tce.to_json(full_filename)
-            
-        #Write metrics    
-        #print(metrics_list)
-        #for i, metric in enumerate(metrics_list):
-        #    metricfilename = "tic%09i-%02i-%s-vetting.json" % (ticid, 
-        #                                            i+1, run_tag)
-        #    full_filename = out_dir + target_dir + metricfilename
-        #    thejson = json.dumps(metric)
-        #    mobj = open(full_filename,'w+')
-        #    mobj.write(thejson)
-        #    mobj.close()
- 
+            with fs.open(full_filename, 'w') as fp:
+                json.dump(tce, fp, cls=JsonCustomEncoder)
 
-        log_obj = open(log_name, 'w+')
-        log_obj.write("Success.")
-        log_obj.close()
+        with fs.open(log_name, 'w') as fp:
+            fp.write("Success!")
 
     except Exception as e:
-        log_obj = open(log_name,'w+')
-        log_obj.write("Failed to create TCEs for TIC %i for Sector %i \n" % (ticid, sector))
-        log_obj.write(str(e))
-        log_obj.close() 
+        with fs.open(log_name,'w') as fp:
+            fp.write("Failed to create TCEs for TIC %i for Sector %i \n" % (ticid, sector))
+            fp.write(str(e))
+            return ["failed", output_file, str(e)]
 
 def load_def_config():
     """
@@ -131,10 +136,8 @@ def load_def_config():
     
     config = dict()
     
+    # Here were the inherited defaults
     config = {
-        "det_window" : 95,  #window used for detrending
-        "noise_window" : 19, #window used for running outlier rejection
-        "n_sigma" : 4.5,  #noise/outlier reject sigma
         "max_period_days" : 11,
         "min_period_days" : 0.8,
         "bls_durs_hrs" : [1,2,4,8,12,14],
@@ -144,32 +147,3 @@ def load_def_config():
         }
     
     return config
-    
-def load_def_vetter():
-    """
-    Load default vetter list of vetters to run.
-    """
-    
-    vetter_list = [vetters.Lpp(),
-                   vetters.OddEven(),
-                   vetters.TransitPhaseCoverage(),
-                   vetters.Sweet()]
-    
-    return vetter_list
-
-def load_def_thresholds():
-    """
-    Load a dictionary of the default threshold values for the vetters.
-
-    Returns
-    -------
-    thresholds : dict
-
-    """
-    thresholds = {'snr' : 1,
-              'norm_lpp' : 2.0,
-              'tp_cover' : 0.6,
-              'oe_sigma' : 3,
-              'sweet' : 3}
-
-    return thresholds
